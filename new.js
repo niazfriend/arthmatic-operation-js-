475d0e2970c0ba20b65a603a3f3d7d43ba0571e0
niazfriend <niazboghri@gmail.com>
2016-09-23 11:32:17 +0500

    arthmatic operation

---
+++ new.js
@@ -0,0 +1,46 @@
+  //starting arthmatic  operation / expression...=================
+  
+  
+  //addition sector
+function plus()
+{
+var a = parseInt(prompt("enter first number"));
+var b = parseInt(prompt("enter second number"));
+var c = a + b;
+alert(c);
+}
+//minus section...
+function minus()
+{
+    var a = parseInt(prompt("enter first number"));
+    var b = parseInt( prompt("enter your second number"));
+    var c = a - b;
+    alert(c)
+ }
+ //divide section ...
+ function division()
+ {
+     var a = parseInt(prompt("enter your dividing first number"));
+     var b = parseInt(prompt("enter your seccond number"));
+     var c = a / b;
+     alert(c);
+  }
+  // here is the multiply section...
+ 
+function multiply()
+{
+    var a = parseInt(prompt("enter your number"));
+    var b = parseInt(prompt("enter your second number"));
+    var z = a * b;
+    alert(z);
+}
+//mode section ...
+function modulos()
+{
+    var niaz = parseInt(prompt("enter your first number"));
+    var ali = parseInt(prompt("enter your second number"));
+    var boghri = niaz % ali;
+    alert(boghri);
+}
+
+//end of arthmatic expressions ....=====================
